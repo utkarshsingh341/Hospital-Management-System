22f9b6adfbd5ddd407a0805b6defc3747d713910
Utkarsh <utkarshsingh341@gmail.com>
2021-01-10 22:41:37 +0530

    Dashboard Edits

--- patient.php
+++ patient.php
@@ -172,39 +172,50 @@ Please fill your account details to complete your registration. <a style="cursor
 ?>
 
 
-
-  <div class="my-3 p-3 bg-white rounded shadow-sm">
-    <h6 class="border-bottom pb-2 mb-0">Recent updates</h6>
-    <div class="d-flex text-muted pt-3">
-      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-
-      <p class="pb-3 mb-0 small lh-sm border-bottom">
-        <strong class="d-block text-gray-dark">@username</strong>
-        Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-      </p>
+<div class="container" style="">
+  <div class="row" style="width:100%">
+  <div class="col-3 bg-light">
+    
+  <div class="card fs-6" style="">
+    <div class="card-body">
+      <h5 class="card-title">Patient Dashboard</h5>
+      <p class="card-text">You have logged in on this Hospital Management System with the role of a <b>Patient</b>. You can Book Appointments, View your Appointment Reports, Add your Medical History for doctors to view.</p>
     </div>
-    <small class="d-block text-end mt-3">
-      <a href="#">All updates</a>
-    </small>
+    <div class="card-body">
+      <a href="#" class="card-link">&#10149; View all features here</a>
+    </div>
   </div>
 
-  <div class="my-3 p-3 bg-white rounded shadow-sm">
-    <h6 class="border-bottom pb-2 mb-0">Suggestions</h6>
-    <div class="d-flex text-muted pt-3">
-      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-
-      <div class="pb-3 mb-0 small lh-sm border-bottom w-100">
-        <div class="d-flex justify-content-between">
-          <strong class="text-gray-dark">Full Name</strong>
-          <a href="#">Follow</a>
-        </div>
-        <span class="d-block">@username</span>
-      </div>
-    </div>
-    <small class="d-block text-end mt-3">
-      <a href="#">All suggestions</a>
-    </small>
   </div>
+  <div class="col-sm-9 bg-light">
+    
+
+
+  <div class="card fs-6">
+    <div class="card-header">
+      Featured
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Appointments Section</h5>
+      <p class="card-text">Book appointments with Doctors & View its status &bull; View Reports of the appointments you've attended!</p>
+      <a href="#" class="btn btn-primary btn-sm">Visit Appointments Page</a>
+    </div>
+  </div>
+
+  <div class="card fs-6">
+    <div class="card-body">
+      <h5 class="card-title">Medical History Dashboard</h5>
+      <p class="card-text">Add your Medical History for doctors to view before appointments &bull; View previously uploaded documents</p>
+      <a href="#" class="btn btn-primary btn-sm">Go to dashboard</a>
+    </div>
+  </div>
+
+  </div>
+
+  </div>
+</div>
+  
+
 </main>
 
 <?php
